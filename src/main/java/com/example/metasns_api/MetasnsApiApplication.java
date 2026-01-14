780a0ce90fdf4043b5af11bf05eab0abab39486b
leunbin <silverbin2105@gmail.com>
2026-01-14 23:12:45 +0900

    feat. test environment setting

---
+++ src/main/java/com/example/metasns_api/MetasnsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.metasns_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetasnsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetasnsApiApplication.class, args);
+	}
+
+}
